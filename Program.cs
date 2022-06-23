d01b4818f0525acbe54293bd1ff6f64faabf2c91
drjackherer <mongkolpersonal@gmail.com>
2022-06-23 07:50:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExceptionHandlingDemo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 0;
+
+            Console.WriteLine("Please enter a number to divide");
+            var userDivide = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a number to divide by?");
+            var divideBy = int.Parse(Console.ReadLine());
+
+            try
+            {
+                answer = userDivide / divideBy;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+
+            }
+            
+            finally
+            {
+                Console.WriteLine("This block will always execute the code");
+            }
+                Console.WriteLine($"The answer is {answer}");
+
+        }
+    }
+}
